872a928fef0efc0b07231c3e33520240bb8238f7
张云鹏 <zhangyunpeng@twt.edu.cn>
2017-04-13 04:09:44 +0800

    Update configbak.php

--- coder.twtstudio.com/twt/Common/Conf/configbak.php
+++ coder.twtstudio.com/twt/Common/Conf/configbak.php
@@ -1,10 +1,9 @@
 <?php
 return array(
-    //远程测试服务器数据库配置
-    'DB_TYPE' => 'mysql', //数据库类型
-    'DB_NAME' => 'coder', //数据库名
-    'DB_USER' => 'coder', //连接数据库帐号
-    'DB_PWD' => 'JJkjxJTP!SJk-Xj989Q', //连接数据库密码
+    'DB_TYPE' => 'mysqli', //数据库类型
+    'DB_HOST' => '202.113.13.173',
+    'DB_NAME' => 'com_twt_coder', //数据库名
+    'DB_USER' => 'com_twt_coder', //连接数据库帐号
+    'DB_PWD' => 'qupCrJl3DAUz', //连接数据库密码
     'DB_PREFIX' => 'tp_' //数据库表前缀，这样在后面实例化的时候就不需要再填写同
-
 );
