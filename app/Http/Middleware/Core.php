3e6ccef9bc8a2dcee26b235c3c9604a083517c44
王海弛 <wanghaichi@twt.edu.cn>
2017-10-28 20:07:01 +0800

    Update Core.php

--- app/Http/Middleware/Core.php
+++ app/Http/Middleware/Core.php
@@ -17,7 +17,7 @@ class Core
     {
         $response = $next($request);
         $response->header('Access-Control-Allow-Origin', '*');
-        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-request-with');
+        //$response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-request-with');
         $response->header('Access-Control-Allow-Methods', 'GET,POST');
         return $response;
     }
