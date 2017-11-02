570b9a978842141a3c26a6d14cc1364671ace9b9
zhangwenwen <792855814@qq.com>
2017-11-02 21:16:44 +0800

    test

--- app/Http/Controllers/ManagerController.php
+++ app/Http/Controllers/ManagerController.php
@@ -181,6 +181,7 @@ class ManagerController extends Controller
             $team->tag = $request->input('tag');
             $team->school = $request->input('school');
             $team->grade = $request->input('grade');
+            dd($team);
             if($request->input('optionsRadios')!=null)
             {
                 $team->type = $request->input('optionsRadios');
